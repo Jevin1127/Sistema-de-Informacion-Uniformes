26a8851d531d00164b5d6f2f6ecefd744af0cf97
Jevin López <95322525+Jevin1127@users.noreply.github.com>
2025-06-24 20:16:29 -0600

    Se añadieron carpetas en DB

---
+++ Test/Config/Config.php
@@ -0,0 +1,13 @@
+<?php
+const base_url = "http://localhost/Test/";
+const host = "localhost:3306";
+const user = "root";
+const pass = "";
+const db = "sistema";
+const charset = "charset=utf8";
+const title = "Uniformes del atlantico";
+const host_smpt = "smtp.gmail.com";
+const user_smtp = "uniformesdelatlantico67@gmail.com";
+const clave_smtp = "slez rguy comz epan";
+const puerto_smtp = 465;
+?>
